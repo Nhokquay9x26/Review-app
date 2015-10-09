93f696a395dc1889ab4ef9afc51676491f19b66f
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-10 01:36:49 +0700

    Create Project AppReviewAsianTech

---
+++ app/src/main/java/asiantech/nhokquay9x26/appreviewasiantech/models/BaseModel.java
@@ -0,0 +1,7 @@
+package asiantech.nhokquay9x26.appreviewasiantech.models;
+
+/**
+ * Created by nhokquay9x26 on 10/10/15.
+ */
+public class BaseModel {
+}
---
+++ app/src/main/java/asiantech/nhokquay9x26/appreviewasiantech/network/Api.java
@@ -0,0 +1,7 @@
+package asiantech.nhokquay9x26.appreviewasiantech.network;
+
+/**
+ * Created by nhokquay9x26 on 10/10/15.
+ */
+interface Api {
+}
