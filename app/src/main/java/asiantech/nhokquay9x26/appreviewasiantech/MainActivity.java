b906797d72098148ad400747a45c6d41911d70fa
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-09 18:49:07 +0700

    Create Project AppReviewAsianTech

---
+++ app/src/main/java/asiantech/nhokquay9x26/appreviewasiantech/MainActivity.java
@@ -0,0 +1,10 @@
+package asiantech.nhokquay9x26.appreviewasiantech;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+}
